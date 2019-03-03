2baf26bdf2f82c8296a08790ab37f7758b4b42c0
MyChusterChuster <38526359+MyChusterChuster@users.noreply.github.com>
2019-03-03 19:39:51 +0100

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -1,8 +1,8 @@
 <?php
-  $_name = htmlspecialchars($_POST['name_in']);
-  $_email= $_POST['email_in'];
-  $_reason= $_POST['group'];
-  $_comment= $_POST['comment_textarea'];
+  $_name = htmlspecialchars($_REQUEST['name_in']);
+  $_email= $_REQUEST['email_in'];
+  $_reason= $_REQUEST['group'];
+  $_comment= $_REQUEST['comment_textarea'];
   $_text="";
   if(isset($_comment)){
     $_text +="Hola soy " .$_name."\n".$_comment."\nPara contactar conmigo hágalo a través de este email: ".$_email;
