9cabf5f82dcfc9a7196dc87fdbc16ab2674aefe1
MyChusterChuster <38526359+MyChusterChuster@users.noreply.github.com>
2019-03-03 19:42:09 +0100

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -4,14 +4,10 @@
   $_reason= $_REQUEST['group'];
   $_comment= $_REQUEST['comment_textarea'];
   $_text="";
-  if(isset($_comment)){
-    $_text +="Hola soy " .$_name."\n".$_comment."\nPara contactar conmigo hágalo a través de este email: ".$_email;
-    $_text += $_name;
-    $_text += "\n";
-    $_text += $_comment;
+  $_text +="Hola soy " .$_name."\n".$_comment."\nPara contactar conmigo hágalo a través de este email: ".$_email;
+  $_text += $_name;
+  $_text += "\n";
+  $_text += $_comment;
 
-    echo' <a href="mailto:mychustercars@gmail.com?subject='.$_reason.'&body='.$_text.'</a>';
-  }else{
-    alert("¡No has introducido el texto a enviar!");
-  }
+  echo' <a href="mailto:mychustercars@gmail.com?subject='.$_reason.'&body='.$_text.'</a>';
  ?>
