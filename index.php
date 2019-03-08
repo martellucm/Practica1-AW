e82a938aea44cc7daa7da1ebab25f9caceb6ab99
mychusterTheVTC <38135870+mychusterTheVTC@users.noreply.github.com>
2019-03-08 12:06:38 +0100

    Cabecera e index php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="es">
+  <head>
+    <title>My Chuster Games</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+    <meta charset="utf-8">
+  </head>
+<body>
+  <div id ="contenedor">
+    <?php require'cabecera.php'?>
+    <div id ="descIndex">
+      <h1> Bienvenido a MyChusterGames</h1>
+      <img src="img/Logo.svg">
+      <p>Desde esta página web podrás valorar y encontrar los enlaces a tus juegos de mesa favoritos. Así mismo, podrás participar diariamente en los mejores torneos y competir contra el resto de usuarios de nuestra web en nuestras tiendas asociadas. Al ganar recibirás puntos que te permitirán ascender por los diferentes rangos de nuestro foro. Los viernes se realizarán los "Viernes de Boquerón" donde se jugará al juego más votado de la semana y donde se elegirá al campeón. </p>
+    </div>
+  </div>
+</body>
+</html>
+
