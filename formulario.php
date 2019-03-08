5cc41bbbd9b0ad9dc61cf80694c0bd7cb3d1f817
MyChusterChuster <38526359+MyChusterChuster@users.noreply.github.com>
2019-03-08 10:49:30 +0100

    Funcionando formularios

--- formulario.php
+++ formulario.php
@@ -1,13 +1,21 @@
 <?php
-  $_name = htmlspecialchars($_REQUEST['name_in']);
-  $_email= $_REQUEST['email_in'];
-  $_reason= $_REQUEST['group'];
-  $_comment= $_REQUEST['comment_textarea'];
-  $_text="";
-  $_text +="Hola soy " .$_name."\n".$_comment."\nPara contactar conmigo hágalo a través de este email: ".$_email;
-  $_text += $_name;
-  $_text += "\n";
-  $_text += $_comment;
+  $_name = htmlspecialchars($_POST['name_in']); //Evitamos los caracteres especiales
+  $_email= $_POST['email_in'];
+  $_reason= $_POST['group'];
+  $_comment= $_POST['comment_textarea'];
+  $_filename = 'formuExit.txt';
 
-  echo' <a href="mailto:mychustercars@gmail.com?subject='.$_reason.'&body='.$_text.'"</a>';
+	if(isset($_POST['check_terms'])){ //Comprobamos que el checkbox esté activado.
+	  /*Creamos una plantilla para darle un formato legible*/
+	  $_text="";
+	  $_text .="Mi nombre es " .$_name." me comunico con ustedes con el asunto '".$_reason."'\n".$_comment."\nPara contactar conmigo hágalo a través de este email: ".$_email;
+
+	  /*Metemos en el fichero nombrado la plantilla generada.*/
+	  file_put_contents($_filename, $_text);
+	}
+	else{
+		echo'<script type="text/javascript">alert("¡HAY QUE ACEPTAR LOS TÉRMINOS!");</script>'; //Ejecutamos un script de información
+	}
+
+	echo'<h1><a href="contacto.html">Return</a></h1>'; //Retornamos a la página de contacto
  ?>
