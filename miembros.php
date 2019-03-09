a93b7b0c80b0c1349cf899b3efcf9d472df958d1
MyChusterChuster <38526359+MyChusterChuster@users.noreply.github.com>
2019-03-09 20:26:03 +0100

    Fotos añadidas

--- miembros.php
+++ miembros.php
@@ -20,14 +20,20 @@
       </div>
       <div class="descpMiem">
         <h3 id="danId"> Daniel Grado Guerrero </h3>
-          <p>  Correo:<a href="mailto:danigrad@ucm.es" target="_top">danigrad@ucm.es</a> </p>
+          <div class="mail_photo">
+            <img src="img/p_photos/daniel_carnet.jpg">
+            <p>  Correo:<a href="mailto:danigrad@ucm.es" target="_top">danigrad@ucm.es</a> </p>
+          </div>
           <p>
             Los gustos de Daniel se definen por una auténtica pasión por los trenes. Este miembro de nuestra empresa
             es miembro honorario de la Federación Española de Asociaciones de Amigos del Ferrocarril. Cuenta con la
             mayor colección de tipos de lirios, la cual puedes encontrar en su propio jardín botánico.
           </p>
         <h3 id ="jesId"> Jesús Álvarez Coll </h3>
-          <p>  Correo:<a href="mailto:jealvare@ucm.es" target="_top">jealvare@ucm.es</a> </p>
+          <div class="mail_photo">
+            <img src="img/p_photos/jesus_carnet.jpg">
+            <p>  Correo:<a href="mailto:jealvare@ucm.es" target="_top">jealvare@ucm.es</a> </p>
+          </div>
           <p>
             Tirador profesional de "Tiro al Nokia", curtido en los campeonatos Irlandeses de Waterford. Este miembro
             es conocido por liderar la empresa MyChuster S.A., en la que, se especializa dentro del campo de investigación
@@ -35,21 +41,30 @@
             todo desafío.
           </p>
         <h3 id="migId"> Miguel Artell Moreno </h3>
-          <p>  Correo:<a href="mailto:martell@ucm.es" target="_top">martell@ucm.es</a> </p>
+          <div class="mail_photo">
+            <img src="img/p_photos/miguel_carnet.jpg">
+            <p>  Correo:<a href="mailto:martell@ucm.es" target="_top">martell@ucm.es</a> </p>
+          </div>
           <p>
             Sus aficiones más destacadas son RRHH (Recursos Humanos) y afición a la colección de monederos. Creador de
             "MyChuster - Sketch" donde puedes sacar toda tu creatividad diseñando las imágenes más creativas. Gran admirador
             de la pizza hawaiana.
           </p>
         <h3 id="ritId"> Rita Estevez Rodriguez </h3>
-          <p>  Correo:<a href="mailto:ritaeste@ucm.es" target="_top">ritaeste@ucm.es</a> </p>
+          <div class="mail_photo">
+            <img src="img/p_photos/rita_carnet.jpg">
+            <p>  Correo:<a href="mailto:ritaeste@ucm.es" target="_top">ritaeste@ucm.es</a> </p>
+          </div>
           <p>
             Este miembro de nuestra empresa toca el ukelele en una banda llamada "Los Anchoas Voladores" donde toca todos
             los viernes en La Tabacalera. Dispone de ni más ni menos que 28 tatuajes gracias a los descuentos del Black Friday.
             Su color favorito es el rosa y se desvive por él.
           </p>
         <h3 id="vicId"> Victor Tello Carrascal </h3>
-          <p>  Correo:<a href="mailto:victello@ucm.es" target="_top">victello@ucm.es</a> </p>
+          <div class="mail_photo">
+            <img src="img/p_photos/victor_carnet.jpg">
+            <p>  Correo:<a href="mailto:victello@ucm.es" target="_top">victello@ucm.es</a> </p>
+          </div>
           <p>
             Su mayor afición es realizar instrucciones de vuelo, para ver brillar los ojos de sus alumnos al volar entre las nubes.
             Dispone de n-1 costillas con las que puede ser más ágil a la hora de jugar a su deporte favorito: el baloncesto. Él mismo
