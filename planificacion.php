b9949fe9540abb9b3ce39a3c6898b3c528f4c15a
retaHater <riestever@gmail.com>
2019-03-09 12:37:49 +0100

    Update planificacion.php

---
+++ planificacion.php
@@ -0,0 +1,45 @@
+<html  lang="es">
+  <head>
+  	<title>My Chuster Games - Planificación </title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+    <meta charset="utf-8">
+  </head>
+<body>
+	<div id ="contenedor">
+    <?php require'cabecera.php'?>
+    	<div id="contenido">
+
+    		//Breve descripción de lo hecho y repartido
+    <TABLE BORDER>
+		<TR>
+		<TH>Objetivo</TH> <TH>Participantes</TH> <TH>Fecha</TH>
+	</TR>
+	<TR>
+		<TD>Creación del proyecto</TD> <TD>Todo el equipo</TD> <TD>C</TD>
+	</TR>
+	<TR>
+		<TD>Entrega</TD> <TD>E</TD> <TD>F</TD>
+	</TR>
+	<TR>
+		<TD>D</TD> <TD>E</TD> <TD>F</TD>
+	</TR>
+	<TR>
+		<TD>D</TD> <TD>E</TD> <TD>F</TD>
+	</TR>
+	<TR>
+		<TD>D</TD> <TD>E</TD> <TD>F</TD>
+	</TR>
+	<TR>
+		<TD>D</TD> <TD>E</TD> <TD>F</TD>
+	</TR>
+	<TR>
+		<TD>D</TD> <TD>E</TD> <TD>F</TD>
+	</TR>
+	<TR>
+		<TD>D</TD> <TD>E</TD> <TD>F</TD>
+	</TR>
+</TABLE>	
+    	</div>
+	</div>
+</body>
+  </html>
