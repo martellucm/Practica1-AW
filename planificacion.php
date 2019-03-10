44787eb5240a764ea62f60eeee9aebf4664425ac
retaHater <riestever@gmail.com>
2019-03-10 19:43:20 +0100

    planificacion + estilo css

--- planificacion.php
+++ planificacion.php
@@ -10,153 +10,152 @@
     	<div id="contenido">
 
     		<h1>Tabla de planificación: </h1>
-    <h3> En este apartado, mostramos una breve título y desglose de las entregas y como hemos repartido los apartados. En clase comentamos que parte nos gustaría realizar a cada uno, y procedemos a valorarlo los domingos en una sesión de Skype, intentando ser lo más justos. Como veremos a continuación hemos hecho una pequeña retrospección a las primeras entregas de documentos de la práctica para mostrar una continuidad en nuestro proyecto. Asimismo nuestra página se estructura en los siguientes apartados: 
-    </h3>
-    	<p>
-    		1. Usuarios (cliente y admin).
-    		2. Organización Torneos.
-    		3. Resultado Torneos.
-    		4. Producto.
-    		5. Foro.
-    	</p>
+    <p>  En este apartado, mostramos una breve título y desglose de las entregas y como hemos repartido los apartados. En clase comentamos que parte nos gustaría realizar a cada uno, y procedemos a valorarlo los domingos en una sesión de Skype, intentando ser lo más justos. Como veremos a continuación hemos hecho una pequeña retrospección a las primeras entregas de documentos de la práctica para mostrar una continuidad en nuestro proyecto. Asimismo nuestra página se estructura en los siguientes apartados: 
+    </p>
+    		<p> <div class="imytext" >
+    		1. Usuarios (cliente y admin).</p> 
+    		   El organizador de este punto es Miguel Artell, <a href="mailto:martell@ucm.es" target="_top">martell@ucm.es </a> tendrá que crear la plataforma de "perfil" tanto para los administradores como para los usuarios de la página explicada más minuciosamente en el apartado detalles. Ambos tipos de perfil tendrán que constar en la base de datos de nuestro sistema. 
+    		</p></div> 
+    	<p><div class="imytext" >
+    		2. Organización Torneos.</p> <p>
+			El organizador de este punto es Daniel Grado,<a href="mailto:danigrad@ucm.es" target="_top">danigrad@ucm.es</a> tendrá que crear la plataforma de "torneo", abordando tareas como la creación e inscripción de los distintos tipos de torneos (véase detalles) con sus respectivas restricciones. Cada torneo deberá constar en la base de datos de nuestro sistema.
+		</p></div>
+		<p><div class="imytext" >
+    		3. Ranking/ Resultado Torneos.</p> <p>
+    		La organizadora de este punto es Rita Estévez,<a href="mailto:ritaeste@ucm.es" target="_top">ritaeste@ucm.es</a> tendrá que crear la formulario que se genera al finalizar o en el transcurso del torneo, para que cada participante pueda introducir su posicionamiento en el torneo. Asimismo se actualizarán tanto las páginas de ranking del mes y global. El ranking formará parte de nuestra base de datos.
+    	</p></div>
+          <p> <div class="imytext" >
+    		4. Producto. </p> <p>
+    		El organizador de este punto es Jesús Álvarez,<a href="mailto:jealvare@ucm.es" target="_top">jealvare@ucm.es</a> tendrá que dar soporte a la parte comercial de nuestro proyecto, los productos. Deberá crear la página inicial que será una lista ordenable de los juegos que están disponibles en nuestra página así como las descripciones, valoraciones y demás. Los productos deberán constar en nuestra base de datos. 
+    	</p></div>
+    	<p> <div class="imytext" >
+    		5. Foro. </p> <p>
+    		El organizador de este punto es Victor Tello, <a href="mailto:victello@ucm.es" target="_top">victello@ucm.es</a> 
+    		tendrá que dar soporte al foro que deseamos implementar, pese a que nos han sugerido reutilizar un foro de otra página, nos parece importante darle nuestro toque personal. Constará de diferentes secciones para los diferentes tópicos que se discutirán en él. Todo lo incluido en el foro constará también en nuestra base de datos. 
 
-    <TABLE BORDER = 10 CELLPADDING=10 CELLSPACING=10>
-		<TR ALIGN=center> </TR>
+    	</p> </div>
+
+   <div class="imytext">
+		<h2> ¡¡Esta información está sujeta a cambios según vaya progresando el proyecto!!</h2>
+	</div>
+    <TABLE>
+	<TR>
 		<TH>Objetivo</TH>
 		<TH>Desglose</TH>
 		<TH>Fecha</TH>
 		<TH>Comentarios</TH>
 		<TH>Participantes</TH>
 	</TR>
-	<TR ALIGN=center>
-		
+	<TR>
 		<TD ROWSPAN=3 COLSPAN = 1>Creación del proyecto</TD>
 	    <TD>Aportación de ideas</TD>
-		<TD>fecha</TD>
-		<TD>bla</TD>
+		<TD>Inicio curso</TD>
+		<TD>Consenso común</TD>
 		<TD>Todo el equipo</TD>
 	</TR>
-		<TR ALIGN=center>
+	<TR>
 	    <TD>Diseño de logos</TD>
-		<TD>fecha</TD>
-		<TD>bla</TD>
+		<TD>Inicio curso</TD>
+		<TD>Imagen por Rita Estévez</TD>
 		<TD>Todo el equipo</TD>
 	</TR>
-		<TR ALIGN=center>
+	<TR>	
 		<TD >Propuesta de nombres</TD>
-		<TD>fecha</TD>
-		<TD>blah</TD>
+		<TD>Inicio curso</TD>
+		<TD>Consenso común</TD>
 		<TD>Todo el equipo</TD>
 	</TR>
 
 	<TR>
 		<TD ROWSPAN =1>Propuesta de proyecto</TD>
-		<TD>E</TD>
-		<TD>Domingo 15 Febrero</TD>
-		<TD>C</TD>
+		<TD>Documento con nuestra idea de proyecto</TD>
+		<TD>15/02/19</TD>
+		<TD>Redactada por Daniel Grado</TD>
+		<TD>Todo el equipo</TD>
 	</TR>
 	<TR>
 		<TD>Propuesta de proyecto revisada</TD>
-		<TD>E</TD>
-		<TD>Martes 19 Febrero</TD>
-		<TD>C</TD>
+		<TD>Documento con nuestra idea de proyecto</TD>
+		<TD>19/02/19</TD>
+		<TD>Redactada por Daniel Grado</TD>
+		<TD>Todo el equipo</TD>
+
 	</TR>
-	<TR ALIGN=center>
-		
+	<TR>
 		<TD ROWSPAN=8 COLSPAN = 1>Entrega Práctica 1</TD>
 	    <TD>Reparto de tareas</TD>
-		<TD>fecha</TD>
-		<TD>bla</TD>
+		<TD>10/03/19</TD>
+		<TD>-</TD>
 		<TD>Todo el equipo</TD>
 	</TR>
-		<TR ALIGN=center>
+	<TR>
 	    <TD>Redacción Index.html</TD>
-		<TD>fecha</TD>
-		<TD>bla</TD>
-		<TD>Todo el equipo</TD>
+		<TD>10/03/19</TD>
+		<TD>-</TD>
+		<TD>Víctor Tello Carrascal</TD>
 	</TR>
-		<TR ALIGN=center>
+	<TR>
 		<TD>Redacción detalles.html</TD>
-		<TD>fecha</TD>
-		<TD>blah</TD>
-		<TD>Todo el equipo</TD>
+		<TD>10/03/19</TD>
+		<TD>-</TD>
+		<TD>Daniel Grado Guerrero</TD>
 	</TR>
-		</TR>
-		<TR ALIGN=center>
+	<TR>
 		<TD>Redacción bocetos.html</TD>
-		<TD>fecha</TD>
-		<TD>blah</TD>
+		<TD>10/03/19</TD>
+		<TD>-</TD>
 		<TD>Miguel Artell Moreno</TD>
 	</TR>
-		</TR>
-		<TR ALIGN=center>
+	<TR>
 		<TD>Redacción miembros.html</TD>
-		<TD>fecha</TD>
-		<TD>blah</TD>
-		<TD>somebody</TD>
+		<TD>10/03/19</TD>
+		<TD>-</TD>
+		<TD>Jesús Álvarez Coll</TD>
 	</TR>
-		</TR>
-		<TR ALIGN=center>
+	<TR>
 		<TD>Redacción planificación.html</TD>
-		<TD>fecha</TD>
-		<TD>blah</TD>
+		<TD>10/03/19</TD>
+		<TD>-</TD>
 		<TD>Rita Estévez Rodríguez</TD>
 	</TR>
-		</TR>
-		<TR ALIGN=center>
+	<TR>
 		<TD>Redacción conctacto.html</TD>
-		<TD>fecha</TD>
-		<TD>blah</TD>
-		<TD>somebody</TD>
+		<TD>10/03/19</TD>
+		<TD>-</TD>
+		<TD>Jesús Álvarez Coll</TD>
 	</TR>
-		</TR>
-		<TR ALIGN=center>
+	<TR>
 		<TD>Redacción detalles.html</TD>
-		<TD>fecha</TD>
-		<TD>blah</TD>
-		<TD>somebody</TD>
+		<TD>10/03/19</TD>
+		<TD>-</TD>
+		<TD>Daniel Grado Guerrero</TD>
 	</TR>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	<TR>
 		<TD>Entrega práctica 2</TD>
-		<TD>E</TD> 
+		<TD>Enunciado pendiente</TD> 
 		<TD>Domingo 24 Marzo</TD>
-		<TD>C</TD>
+		<TD>-</TD>
+		<TD>Todo el equipo</TD>
 	</TR>
 	<TR>
 		<TD>Entrega práctica 3</TD>
-		<TD>E</TD>
+		<TD>Enunciado pendiente</TD>
 		<TD>Jueves 11 Abril</TD>
-		<TD>C</TD>
+		<TD>-</TD>
+		<TD>Todo el equipo</TD>
 	</TR>
 	<TR>
 		<TD>Entrega proyecto final</TD>
-		<TD>E</TD>
+		<TD>Enunciado pendiente</TD>
 		<TD>Lunes 27 Mayo</TD>
-		<TD>C</TD>
+		<TD>-</TD>
+		<TD>Todo el equipo</TD>
 	</TR>
-	<TR>
-		<TD>D</TD>
-		<TD>E</TD>
-		<TD>F</TD>
-		<TD>C</TD>
-	</TR>
+	
 </TABLE>	
-    	</div>
+    </div>
 	</div>
+
+	
 </body>
   </html>
