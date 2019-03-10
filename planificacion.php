f5ee621275479e05e813ecd1a5daf620e08bc4b2
retaHater <riestever@gmail.com>
2019-03-10 17:27:25 +0100

    Update planificación.php

--- planificacion.php
+++ planificacion.php
@@ -9,34 +9,151 @@
     <?php require'cabecera.php'?>
     	<div id="contenido">
 
-    		//Breve descripción de lo hecho y repartido
-    <TABLE BORDER>
-		<TR>
-		<TH>Objetivo</TH> <TH>Participantes</TH> <TH>Fecha</TH>
+    		<h1>Tabla de planificación: </h1>
+    <h3> En este apartado, mostramos una breve título y desglose de las entregas y como hemos repartido los apartados. En clase comentamos que parte nos gustaría realizar a cada uno, y procedemos a valorarlo los domingos en una sesión de Skype, intentando ser lo más justos. Como veremos a continuación hemos hecho una pequeña retrospección a las primeras entregas de documentos de la práctica para mostrar una continuidad en nuestro proyecto. Asimismo nuestra página se estructura en los siguientes apartados: 
+    </h3>
+    	<p>
+    		1. Usuarios (cliente y admin).
+    		2. Organización Torneos.
+    		3. Resultado Torneos.
+    		4. Producto.
+    		5. Foro.
+    	</p>
+
+    <TABLE BORDER = 10 CELLPADDING=10 CELLSPACING=10>
+		<TR ALIGN=center> </TR>
+		<TH>Objetivo</TH>
+		<TH>Desglose</TH>
+		<TH>Fecha</TH>
+		<TH>Comentarios</TH>
+		<TH>Participantes</TH>
+	</TR>
+	<TR ALIGN=center>
+		
+		<TD ROWSPAN=3 COLSPAN = 1>Creación del proyecto</TD>
+	    <TD>Aportación de ideas</TD>
+		<TD>fecha</TD>
+		<TD>bla</TD>
+		<TD>Todo el equipo</TD>
+	</TR>
+		<TR ALIGN=center>
+	    <TD>Diseño de logos</TD>
+		<TD>fecha</TD>
+		<TD>bla</TD>
+		<TD>Todo el equipo</TD>
+	</TR>
+		<TR ALIGN=center>
+		<TD >Propuesta de nombres</TD>
+		<TD>fecha</TD>
+		<TD>blah</TD>
+		<TD>Todo el equipo</TD>
+	</TR>
+
+	<TR>
+		<TD ROWSPAN =1>Propuesta de proyecto</TD>
+		<TD>E</TD>
+		<TD>Domingo 15 Febrero</TD>
+		<TD>C</TD>
 	</TR>
 	<TR>
-		<TD>Creación del proyecto</TD> <TD>Todo el equipo</TD> <TD>C</TD>
+		<TD>Propuesta de proyecto revisada</TD>
+		<TD>E</TD>
+		<TD>Martes 19 Febrero</TD>
+		<TD>C</TD>
+	</TR>
+	<TR ALIGN=center>
+		
+		<TD ROWSPAN=8 COLSPAN = 1>Entrega Práctica 1</TD>
+	    <TD>Reparto de tareas</TD>
+		<TD>fecha</TD>
+		<TD>bla</TD>
+		<TD>Todo el equipo</TD>
+	</TR>
+		<TR ALIGN=center>
+	    <TD>Redacción Index.html</TD>
+		<TD>fecha</TD>
+		<TD>bla</TD>
+		<TD>Todo el equipo</TD>
+	</TR>
+		<TR ALIGN=center>
+		<TD>Redacción detalles.html</TD>
+		<TD>fecha</TD>
+		<TD>blah</TD>
+		<TD>Todo el equipo</TD>
+	</TR>
+		</TR>
+		<TR ALIGN=center>
+		<TD>Redacción bocetos.html</TD>
+		<TD>fecha</TD>
+		<TD>blah</TD>
+		<TD>Miguel Artell Moreno</TD>
+	</TR>
+		</TR>
+		<TR ALIGN=center>
+		<TD>Redacción miembros.html</TD>
+		<TD>fecha</TD>
+		<TD>blah</TD>
+		<TD>somebody</TD>
+	</TR>
+		</TR>
+		<TR ALIGN=center>
+		<TD>Redacción planificación.html</TD>
+		<TD>fecha</TD>
+		<TD>blah</TD>
+		<TD>Rita Estévez Rodríguez</TD>
+	</TR>
+		</TR>
+		<TR ALIGN=center>
+		<TD>Redacción conctacto.html</TD>
+		<TD>fecha</TD>
+		<TD>blah</TD>
+		<TD>somebody</TD>
+	</TR>
+		</TR>
+		<TR ALIGN=center>
+		<TD>Redacción detalles.html</TD>
+		<TD>fecha</TD>
+		<TD>blah</TD>
+		<TD>somebody</TD>
+	</TR>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<TR>
+		<TD>Entrega práctica 2</TD>
+		<TD>E</TD> 
+		<TD>Domingo 24 Marzo</TD>
+		<TD>C</TD>
 	</TR>
 	<TR>
-		<TD>Entrega</TD> <TD>E</TD> <TD>F</TD>
+		<TD>Entrega práctica 3</TD>
+		<TD>E</TD>
+		<TD>Jueves 11 Abril</TD>
+		<TD>C</TD>
 	</TR>
 	<TR>
-		<TD>D</TD> <TD>E</TD> <TD>F</TD>
+		<TD>Entrega proyecto final</TD>
+		<TD>E</TD>
+		<TD>Lunes 27 Mayo</TD>
+		<TD>C</TD>
 	</TR>
 	<TR>
-		<TD>D</TD> <TD>E</TD> <TD>F</TD>
-	</TR>
-	<TR>
-		<TD>D</TD> <TD>E</TD> <TD>F</TD>
-	</TR>
-	<TR>
-		<TD>D</TD> <TD>E</TD> <TD>F</TD>
-	</TR>
-	<TR>
-		<TD>D</TD> <TD>E</TD> <TD>F</TD>
-	</TR>
-	<TR>
-		<TD>D</TD> <TD>E</TD> <TD>F</TD>
+		<TD>D</TD>
+		<TD>E</TD>
+		<TD>F</TD>
+		<TD>C</TD>
 	</TR>
 </TABLE>	
     	</div>
