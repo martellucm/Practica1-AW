4cec84a629f6ecf8ce0b69c2af73025889ade954
MyChusterChuster <38526359+MyChusterChuster@users.noreply.github.com>
2019-03-10 14:41:28 +0100

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -1,11 +1,11 @@
 <?php
-  $_name = htmlspecialchars($_POST['name_in']); //Evitamos los caracteres especiales
-  $_email= $_POST['email_in'];
-  $_reason= $_POST['group'];
-  $_comment= $_POST['comment_textarea'];
+  $_name = htmlspecialchars(strip_tags($_POST['name_in'])); //Evitamos los caracteres especiales
+  $_email= trim(strip_tags($_POST['email_in']));
+  $_comment= strip_tags($_POST['comment_textarea']);
   $_filename = 'formuExit.txt';
 
-  if(isset($_POST['enviar'])){
+  if(isset($_POST['enviar']) && isset($_POST['group'])){
+	  $_reason= $_POST['group']; //Si se ha indicado una razón entonces se procesa
   	if(isset($_POST['check_terms']) && $_POST['check_terms'] == 'Yes'){ //Comprobamos que el checkbox esté activado.
   	  /*Creamos una plantilla para darle un formato legible*/
   	  $_text="";
@@ -13,12 +13,15 @@
 
   	  /*Metemos en el fichero nombrado la plantilla generada.*/
   	  file_put_contents($_filename, $_text);
-	    echo'<script type="text/javascript">alert("Todo salió correctamente :)");</script>'; //Ejecutamos un script de información
+	    echo'<script type="text/javascript">alert("Todo salió correctamente :)");</script>'; //Ejecutamos un script de Información
 
   	}
   	else{
-  		echo'<script type="text/javascript">alert("¡HAY QUE ACEPTAR LOS TÉRMINOS!");</script>'; //Ejecutamos un script de información
+  		echo'<script type="text/javascript">alert("¡HAY QUE ACEPTAR LOS TÉRMINOS!");</script>'; //Ejecutamos un script de Error
   	}
+  }
+  else{
+	  echo'<script type="text/javascript">alert("¡NO INDICASTE NINGUNA RAZÓN!");</script>'; //Ejecutamos un script de Error
   }
 
   require('contacto.php'); //Retornamos a la página de contacto
